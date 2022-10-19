1c8305b46634d42aa5e45312551871c15f9e50d0
Leandro Araujo <leandro.rufma@gmail.com>
2022-10-19 15:15:00 -0300

    corrigido configurador dos slots de loot

--- quickloot.py
+++ quickloot.py
@@ -8,7 +8,7 @@ def quickLoot(lootPositions):
 
     for key in lootPositions:
         position = lootPositions[key]
-        ctypes.windll.user32.SetCursorPos(position['x'], position['y'])
+        ctypes.windll.user32.SetCursorPos(int(position['x']), int(position['y']))
         ctypes.windll.user32.mouse_event(0x0008, 0, 0, 0, 0) # left down
         ctypes.windll.user32.mouse_event(0x0010, 0, 0, 0, 0) # left up
         #time.sleep(0.01)
